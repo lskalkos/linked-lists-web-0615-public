5659d14d7fa648578e2dbfea97ab2dfe6608324a
Lia Skalkos <hello@liaskalkos.net>
2015-08-26 10:34:51 -0400

    Specs passing

--- deque/deque.rb
+++ deque/deque.rb
@@ -1,5 +1,69 @@
+require 'pry'
 require_relative '../simple-lists/simple_linked_list'
 
+class Element
+  attr_accessor :datum, :next, :previous
+
+  def initialize(data, next_node)
+    @datum = data
+    @next = next_node
+    next_node.previous = self unless next_node.nil?
+  end
+
+  def next=(next_node)
+    @next = next_node
+    next_node.previous = self if !next_node.nil? && next_node.previous != self
+  end
+
+  def previous=(previous_node)
+    @previous = previous_node
+    previous_node.next = self if !previous_node.nil? && previous_node.next != self
+  end
+
+
+end
+
 class Deque
+  attr_accessor :list, :last
+
+
+  def push(data)
+    if self.list.nil?
+      self.list = Element.new(data, nil)
+      self.last = self.list
+    else
+      current = list
+      while !current.next.nil?
+        current = current.next
+      end
+      current.next = Element.new(data, nil)
+      self.last = current.next
+    end
+
+  end
+
+  def pop
+    value = self.last.datum
+    self.last = self.last.previous
+    self.last.next = nil unless self.last.nil?
+    value
+  end
+
+  def shift
+    value = self.list.datum
+    self.list = self.list.next
+    value
+  end
+
+  def unshift(data)
+    if self.list.nil?
+      self.list = Element.new(data, nil)
+      self.last = self.list
+    else
+      self.list.previous = Element.new(data, self.list)
+      self.list = self.list.previous
+      self.last = self.list.next
+    end
+  end
 
 end
--- simple-lists/simple_linked_list.rb
+++ simple-lists/simple_linked_list.rb
@@ -1,6 +1,45 @@
 class Node
+  attr_accessor :data, :next
 
+  def initialize(data, next_node)
+    @data = data
+    @next = next_node
+  end
 end
 
 class LinkedList
+  attr_reader :head
+
+  def initialize(data)
+    @head = Node.new(data, nil)
+  end
+
+  def index_at(position)
+    current = head
+    position.times{ current = current.next }
+    current.nil? ? nil : current.data
+  end
+
+  def insert_at_index(position, data)
+    current = head
+    current.next = Node.new(nil, nil) if current.next.nil?
+    position.times do
+      current = current.next
+      current.next = Node.new(nil, nil) if current.next.nil?
+    end
+    current.next.data = current.data
+    current.data = data
+  end
+
+  def unshift(new_data)
+    insert_at_index(0, new_data)
+  end
+
+  def push(new_data)
+    current = head
+    while !current.next.nil? do
+      current = current.next
+    end
+    current.data = new_data
+  end
 end
